155af2e4f3bfe6095839b98174c9047db5db72ee
jimedevelopers <168291729+jimedevelopers@users.noreply.github.com>
2024-05-03 12:00:29 +0530

    Update __init__.py

--- contosmall/__init__.py
+++ contosmall/__init__.py
@@ -3,20 +3,20 @@ import check50
 
 @check50.check()
 def exists():
-    """indoor.py exists"""
-    check50.exists("indoor.py")
+    """contosmall.py exists"""
+    check50.exists("contosmall.py")
 
 @check50.check(exists)
 def testhello():
     """input of HELLO yields output of hello"""
-    check50.run("python3 indoor.py").stdin("HELLO", prompt=False).stdout("hello").exit()
+    check50.run("python3 contosmall.py").stdin("HELLO", prompt=False).stdout("hello").exit()
 
 @check50.check(exists)
 def testcs50():
-    """input of THIS IS CS50 yields output of this is cs50"""
-    check50.run("python3 indoor.py").stdin("THIS IS CS50", prompt=False).stdout("this is cs50").exit()
+    """input of THIS IS JIME yields output of this is jime"""
+    check50.run("python3 contosmall.py").stdin("THIS IS JIME", prompt=False).stdout("this is jime").exit()
 
 @check50.check(exists)
 def testnumber():
-    """input of 50 yields output of 50"""
-    check50.run("python3 indoor.py").stdin("50", prompt=False).stdout("50").exit()
+    """input of 100 yields output of 100"""
+    check50.run("python3 contosmall.py").stdin("100", prompt=False).stdout("100").exit()
