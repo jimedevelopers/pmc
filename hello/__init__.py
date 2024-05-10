0f3b2b9a044eba626a1a31c0324072ef3e3f9182
jimedevelopers <168291729+jimedevelopers@users.noreply.github.com>
2024-05-10 20:18:39 +0530

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -3,6 +3,9 @@ import check50
 @check50.check()
 
 def exists():
-  """:) hello.py exists"""
+  """hello.py exists"""
   check50.exists("hello.py")
+def output(exists):
+  """We can get Hello World as output (If this is red please type 'Hello World' exactly as shown)"""
+  check50.run("hello.py").stdout("Hello World").exit(0)
   
