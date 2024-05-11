75ba1911c3dc11f293d53ac652800cca7095b486
jimedevelopers <168291729+jimedevelopers@users.noreply.github.com>
2024-05-11 19:58:27 +0530

    Update __init__.py

--- hello_name/__init__.py
+++ hello_name/__init__.py
@@ -1,6 +1,6 @@
-1import check50
-2
-3@check50.check()
-4def id():
-5    """id.py prints what you give it"""
-6    check50.run("python3 hello_name.py").stdin("foo").stdout("hello foo").stdin("bar").stdout("hello bar")
+import check50
+
+@check50.check()
+def id():
+    """id.py prints what you give it"""
+    check50.run("python3 hello_name.py").stdin("foo").stdout("hello foo").stdin("bar").stdout("hello bar")
