6510980c12cac967388bffbc8632915cfc56e08a
jimedevelopers <168291729+jimedevelopers@users.noreply.github.com>
2024-05-19 17:35:23 +0530

    Update __init__.py

--- hello_name/__init__.py
+++ hello_name/__init__.py
@@ -1,6 +1,24 @@
 import check50
 
 @check50.check()
-def id():
-    """id.py prints what you give it"""
-    check50.run("python3 hello_name.py").stdin("foo").stdout("hello foo").stdin("bar").stdout("hello bar")
+def exists():
+    """hello_name.py exists"""
+    check50.exists("hello_name.py")
+
+
+@check50.check(exists)
+def test1():
+    """input of Jameel yields output of Hello Jameel"""
+    output = check50.run("python3 hello_name.py").stdin("Jameel", prompt=False).stdout("[Hh]ello Jameel")
+
+
+@check50.check(exists)
+def test14():
+    """input of Jime yields output of Hello Jime"""
+    output = check50.run("python3 hello_name.py").stdin("Jime", prompt=False).stdout("[Hh]ello Jime")
+
+
+@check50.check(exists)
+def test50():
+    """input of Haebaek yields output of Hello Haebaek"""
+    output = check50.run("python3 hello_name.py").stdin("Haebaek", prompt=False).stdout("Hello Haebaek")
